8d418fd7cad59d3ab38fc669c87ab708f089fc90
djnaSoft <djna01@gmail.com>
2022-08-10 09:19:40 +0100

    softwire.com

--- emailExtract.js
+++ emailExtract.js
@@ -6,10 +6,26 @@ import lineReader from 'line-reader';
 
 const filename = 'sample.txt';
 
-lineReader.eachLine(filename, function(line, last) {
-  console.log(`Line from file: ${line}`);
-  if(last) {
-    console.log('Last line printed.');
-  }
+let emailAddresses = {};
+
+lineReader.eachLine(filename, function (line, last) {
+    let emailRegex = /(\w+)@(softwire.com)\b/g;
+    let match;
+    while (match = emailRegex.exec(line)) {
+        let oneAddress = match[2];
+        let count = emailAddresses[oneAddress] || 0;
+        emailAddresses[oneAddress] = ++count;
+    }
+
+    if (last) {
+        console.log(emailAddresses);
+        const sortedEmails = Object.entries(emailAddresses)
+            .sort(([, countLeft], [, countRight]) => countLeft - countRight);
+        //  .reduce((r, [k, v]) => ({ ...r, [k]: v }), {});
+
+        console.log(sortedEmails);
+    }
 });
+
+
 
